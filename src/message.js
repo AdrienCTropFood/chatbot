f0558f0d1efa840618cf6940e665e8b60b659220
Adrien <adrien@ctropfood.com>
2017-10-12 11:58:47 +0200

    12

--- src/message.js
+++ src/message.js
@@ -44,7 +44,7 @@ const replyMessage = (message) =>
 		{
 			console.log('demande info nutrition')
 			// get all the aliment entities extracted from your text
-			var aliments = result.all('FOOD');
+			var aliments = result.get('FOOD');
 			var aliment = "";
 			aliments.forEach(function(element)
 			{
